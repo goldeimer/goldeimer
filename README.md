d5c3226e60d521b9684908c0ce5c004cdbc2e796
Johannes Pilkahn <j.pilkahn@gmail.com>
2020-04-08 12:47:24 +0200

    Clean up syntax, add .gitignore

--- README.md
+++ README.md
@@ -1,13 +1,4 @@
-# Install Wordpress in Docker with Nginx and Lets Encrypt for HTTPS
+Goldeimer web apps staging env
+===
 
-Wow, that was a mouthful.  But yeah, that's what this repo does.
-
-To get started, after you clone this repo, run `docker-compose up -d` and then visit http://localhost/.
-
-If you are running this on a production server, you'll want to tweak the server name in 
-the HTTPS-Portal section of `docker-compose.yml` accordingly.
-
-This repo is supporting material for the blog post at https://wp.dmuth.org/wordpress-5-in-docker-with-nginx-and-letsencrypt/
-
-
-
+Dockerized local / staging env.
--- reset.sh
+++ reset.sh
@@ -1,26 +1,10 @@
-#!/bin/bash
-#
-# Reset our setup so that we can start over from scratch.
-#
+#!/usr/bin/sh
 
-# Errors are fatal
-set -e
-
-
-echo "# "
 echo "# Stopping and removing containers..."
-echo "# "
-docker-compose kill 
+
+docker-compose kill
 docker-compose rm -f
 
-echo "# "
-echo "# Removing database, wordpress, and logs..."
-echo "# "
-#
-# I made a conscious decision to leave ssl_certs/ untouched, as it can
-# take 10s of seconds to generate them, and I don't want users to have
-# to wait through that on every single iteration, especially for development purposes.
-#
+echo "Removing database, wordpress, and logs..."
+
 rm -rf data/ logs/ wordpress/
-
-
