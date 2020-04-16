6976cfcb189032791a077b7a6f6d4ff0cf127bb3
Johannes Pilkahn <j.pilkahn@gmail.com>
2020-04-17 01:26:58 +0200

    Further semantic split of 'functions.php'

--- src/functions.php
+++ src/functions.php
@@ -10,51 +10,9 @@ if (! defined('GOLDEIMER_THEME_ABSPATH'))
 }
 
 
-require_once(GOLDEIMER_THEME_ABSPATH.'/util/dev-util.php');
-require_once(GOLDEIMER_THEME_ABSPATH.'/util/php-util.php');
-
+require_once(GOLDEIMER_THEME_ABSPATH.'/include/filter-p-bank.php');
 require_once(GOLDEIMER_THEME_ABSPATH.'/shortcode/shortcode-iframe-embed.php');
-
-
-/// ---------------------------- 3rd party filters ----------------------------
-
-// TODO(Johannes):
-// Figure out why this is needed. Document.
-// (Predates my time.)
-function av_change_logo($logo)
-{
-    if (is_page(13893))
-    {
-        return "http://www.goldeimer.de/wp-content/uploads/2019/06/" .
-               "P-Bank_Logo-y.png";
-    }
-
-    return $logo;
-}
-
-add_filter(
-    'avf_logo',
-    'av_change_logo'
-);
-
-
-// TODO(Johannes):
-// Figure out why this is needed. Document.
-// (Predates my time.)
-function av_change_logo_link($link)
-{
-    if (is_page(13893))
-    {
-        return "#welcome";
-    }
-
-    return $link;
-}
-
-add_filter(
-    'avf_logo_link',
-    'av_change_logo_link'
-);
+require_once(GOLDEIMER_THEME_ABSPATH.'/util/dev-util.php');
 
 
 ?>
