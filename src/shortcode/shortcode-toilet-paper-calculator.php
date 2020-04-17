5cc971c76010f9c6044aafa730d9a75e718c7f33
Johannes Pilkahn <j.pilkahn@gmail.com>
2020-04-17 03:43:28 +0200

    Add shortcode for toilet paper calculator

---
+++ src/shortcode/shortcode-toilet-paper-calculator.php
@@ -0,0 +1,44 @@
+<?php
+
+
+# -------------------------- toiletpaper calculator ---------------------------
+
+
+const SCRIPT_HANDLE = 'goldeimer-toilet-paper-calculator-standalone-bundle';
+
+
+function registerToiletPaperCalculator()
+{
+    wp_register_script(
+        SCRIPT_HANDLE,
+        plugins_url(
+            '/js/toilet_paper_calculator_standalone.bundle.js',
+            __FILE__
+        ),
+        array(),
+        '1',
+        true
+    );
+}
+
+
+add_action(
+    'wp_enqueue_scripts',
+    'registerToiletPaperCalculator'
+);
+
+
+function shortcodeToiletPaperCalculator($attributes)
+{
+    wp_enqueue_script(SCRIPT_HANDLE);
+
+    return '<div id="react-app-toilet-paper-calculator"></div>';
+}
+
+add_shortcode(
+    'goldeimer-toilet-paper-calculator',
+    'shortcodeToiletPaperCalculator'
+);
+
+
+?>
