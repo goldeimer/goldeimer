7cb2a8bbd92135bc7f420c203576f78add4bb190
Johannes Pilkahn <j.pilkahn@gmail.com>
2020-05-02 14:20:50 +0200

    feat(merchant-map): Duplicate VcA's merchant data endpoint (not configured for CORS)

---
+++ legacy/api/get-merchants-vca.php
@@ -0,0 +1,36 @@
+<?
+
+require_once('./secrets-merchants-vca.php');
+
+try
+{
+    $dbh = new PDO(
+        DB_VCA_DATA_SOURCE_NAME,
+        DB_VCA_USER,
+        DB_VCA_PASS,
+        array(
+            PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"
+        )
+    );
+}
+catch (Exception $e)
+{
+    print(
+        sprintf(
+            "Error establishing database connection: %s",
+            $e->getMessage()
+        )
+    );
+
+    die();
+}
+
+$sql = $dbh->prepare(
+    "SELECT name, street, city, country, url, x(location) latitude, y(location) longitude from haendler"
+);
+
+$sql->execute();
+
+echo json_encode(
+    $sql->fetchAll(PDO::FETCH_ASSOC)
+);
---
+++ legacy/api/secrets-merchants-vca.sample.php
@@ -0,0 +1,5 @@
+<?
+
+const DB_VCA_DATA_SOURCE_NAME = 'mysql:dsn...';
+const DB_VCA_PASS = 'password';
+const DB_VCA_USER = 'username';
