cb53072a4cb555e93cae2e7ad8286c1c4b6259d0
Johannes Pilkahn <j.pilkahn@gmail.com>
2020-05-02 19:56:10 +0200

    feat(merchant-map): Add react-map-gl, Uber's React wrapper around mapbox-gl-js

--- src/js/components/MerchantMap/MerchantMap.jsx
+++ src/js/components/MerchantMap/MerchantMap.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
-
 import { ThemeProvider } from '@material-ui/core/styles';
+import MapGL from 'react-map-gl';
 
 import muiTheme from 'js/muiTheme';
 
@@ -10,10 +10,21 @@ import {
 } from './getMerchantData';
 
 
+const MAP_STYLE_URL = 'https://api.maptiler.com/maps/dc1364cc-f025-4bac-9773-a5871f2b14eb/style.json?key=ELs001Fn1Ojoa3POXZTf';
+
+
 const MerchantMap = () =>
 {
     const [merchantDataGoldeimer, setMerchantDataGoldeimer] = useState(null);
     const [merchantDataVca, setMerchantDataVca] = useState(null);
+
+    const [viewport, setViewport] = useState({
+        latitude: 51.2,
+        longitude: 10.4,
+        zoom: 5,
+        bearing: 0,
+        pitch: 0
+    });
 
     useEffect(
         () => {
@@ -35,6 +46,14 @@ const MerchantMap = () =>
 
     return (
         <ThemeProvider theme={muiTheme}>
+            <MapGL
+                {...viewport}
+                width="100vw"
+                height="100vh"
+                mapStyle={MAP_STYLE_URL}
+                onViewportChange={(nextViewport) => setViewport(nextViewport)}
+                mapboxApiAccessToken={''}
+            />
             <h1>Merchant Map</h1>
             <code>
                 {
@@ -43,8 +62,6 @@ const MerchantMap = () =>
                     : ''
                 }
             </code>
-            <br />
-            <br />
             <br />
             <code>
                 {
