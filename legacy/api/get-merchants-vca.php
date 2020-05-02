b860c387e58a85cfb2df40ac9367f06c72f2d309
Johannes Pilkahn <j.pilkahn@gmail.com>
2020-05-02 21:08:06 +0200

    improve(merchant-map): Return proper GeoJSON (RFC 7946) from merchant endpoint (VcA)

--- legacy/api/get-merchants-vca.php
+++ legacy/api/get-merchants-vca.php
@@ -2,37 +2,80 @@
 
 require_once('./secrets-merchants-vca.php');
 
-try
+
+/// @see [RFC 7946](https://tools.ietf.org/html/rfc7946)
+function dbRowToGeoJsonFeature($row)
 {
-    $dbh = new PDO(
-        DB_VCA_DATA_SOURCE_NAME,
-        DB_VCA_USER,
-        DB_VCA_PASS,
-        array(
-            PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"
-        )
-    );
+    return [
+        'type' => 'Feature',
+        'geometry' => [
+            'type' => 'Point',
+            'coordinates' => [
+                $row['longitude'],
+                $row['latitude'],
+            ],
+        ],
+        'properties' => [
+            'address' => [
+                'city' => $row['city'],
+                'country' => $row['country'],
+                'street' => $row['street'],
+            ],
+            'brand' => 'vca',  // vs. 'goldeimer'
+            'merchantType' => 'retail',  // vs. 'wholesale'
+            'name' => $row['name'],
+            'url' => $row['url'],
+        ],
+    ];
 }
-catch (Exception $e)
+
+
+function dbResultToGeoJson($result)
 {
-    print(
-        sprintf(
-            "Error establishing database connection: %s",
-            $e->getMessage()
-        )
-    );
-
-    die();
+    return [
+        'type' => 'FeatureCollection',
+        'features' => array_map('dbRowToGeoJsonFeature', $result)
+    ];
 }
 
-$sql = $dbh->prepare(
-    "SELECT name, street, city, country, url, x(location) latitude, y(location) longitude from haendler"
-);
 
-$sql->execute();
+function queryVcaDb()
+{
+    try
+    {
+        $dbh = new PDO(
+            DB_VCA_DATA_SOURCE_NAME,
+            DB_VCA_USER,
+            DB_VCA_PASS,
+            [PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"]
+        );
+    }
+    catch (Exception $e)
+    {
+        print(
+            sprintf(
+                "Error establishing database connection: %s",
+                $e->getMessage()
+            )
+        );
+
+        die();
+    }
+
+    $pdoStatement = $dbh->prepare(
+        "SELECT name, street, city, country, url, x(location) latitude, y(location) longitude from haendler"
+    );
+
+    $pdoStatement->execute();
+
+    return $pdoStatement->fetchAll(PDO::FETCH_ASSOC);
+}
+
 
 header('Content-Type: application/json');
 
 echo json_encode(
-    $sql->fetchAll(PDO::FETCH_ASSOC)
+    dbResultToGeoJson(
+        queryVcaDb()
+    )
 );
