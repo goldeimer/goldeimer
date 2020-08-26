d831b453628c0ccc11c4ca92d761d2f101dec74f
Johannes Pilkahn <j.pilkahn@gmail.com>
2020-08-26 09:55:32 +0200

    feat(abenteuer-regenwald): Integrate with webpack

---
+++ packages/goldeimer-abenteuer-regenwald-campaign/webpack.config.js
@@ -0,0 +1,51 @@
+import {
+    CustomWebpackBrowserSchema,
+    TargetOptions
+} from '@angular-builders/custom-webpack'
+
+const merge = require('webpack-merge')
+const path = require('path')
+
+const {
+    newCopyPlugin,
+    newDefinePlugin,
+    newHtmlPlugin,
+    withBaseConfig
+} = require('@goldeimer-config/webpack')
+
+const pkg = require('./package.json')
+
+const SRC = path.resolve(__dirname, 'src')
+
+module.exports = (
+    config,
+    options,
+    targetOptions
+) => {
+    withBaseConfig({
+        name: '@goldeimer/abenteuer-regenwald-campaign',
+        plugins: [
+            newCopyPlugin({
+                from: path.resolve(
+                    SRC,
+                    'backend-wordpress-plugin'
+                )
+            }),
+            newHtmlPlugin({
+                favicon: path.resolve(
+                    SRC,
+                    'img',
+                    'favicon',
+                    'goldeimer.favicon.png'
+                ),
+                filename: 'webpack.html',
+                primaryColor: '#ffe300',
+                subject: 'Goldeimers "Keinebaeume" Kampagne in Zusammenarbeit mit Abenteuer Regenwald',
+                title: 'Keinebaeume'
+            }),
+            newDefinePlugin({
+                pkg
+            })
+        ]
+    })
+}
