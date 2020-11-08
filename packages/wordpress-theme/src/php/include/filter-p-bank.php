31c205f901bfb56d9f9beda53dbbf2340b571ef7
Johannes Pilkahn <johannes@goldeimer.de>
2020-11-08 19:30:17 +0100

    refactor(wp-theme-goldeimer): isolate the WordPress theme into a separate package, strip the calculator (willl receive package as well), integrate composer

---
+++ packages/wordpress-theme/src/php/include/filter-p-bank.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Goldeimer\WordPressTheme;
+
+const P_BANK_PAGE_ID = 13893;
+
+function isPBankPage()
+{
+    return is_page(P_BANK_PAGE_ID);
+}
+
+/// @brief Displays a non-default logo image on selected page(s).
+function avChangeLogoOnPBankPage($logoImagePath)
+{
+    if (isPBankPage())
+    {
+        return "http://www.goldeimer.de/wp-content/uploads/2019/06/" .
+               "P-Bank_Logo-y.png";
+    }
+
+    return $logoImagePath;
+}
+
+/// @brief Uses a non-default `href` attribute value for the logo link on
+/// selected page(s).
+function avChangeLogoLinkOnPBankPage($link)
+{
+    if (isPBankPage())
+    {
+        return "#welcome";
+    }
+
+    return $link;
+}
+
+add_filter(
+    'avf_logo',
+    'avChangeLogoOnPBankPage'
+);
+
+add_filter(
+    'avf_logo_link',
+    'avChangeLogoLinkOnPBankPage'
+);
