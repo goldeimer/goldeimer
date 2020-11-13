ad18c136ef530aee1eb10307a175bc2290467c3a
Johannes Pilkahn <johannes@goldeimer.de>
2020-11-13 10:56:19 +0100

    feat(wp-theme-goldeimer): integrate ninja forms

--- packages/wordpress-theme/src/wordpress-theme.js
+++ packages/wordpress-theme/src/wordpress-theme.js
@@ -1,9 +1,3 @@
 import '@goldeimer/font-stack'
 
-<<<<<<< Updated upstream
-import './css/parent-theme-overrides.css'
-import './css/main.css'
-import './css/button.scss'
-=======
 import './scss/index.scss'
->>>>>>> Stashed changes
