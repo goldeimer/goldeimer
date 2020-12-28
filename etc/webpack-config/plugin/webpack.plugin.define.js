3511c4ca08aaa54ef8a65370c24951e1cb318a37
Johannes Pilkahn <johannes@goldeimer.de>
2020-12-28 02:02:09 +0100

    fix(js-util): remove superfluous argument from build mode condition functions

--- etc/webpack-config/plugin/webpack.plugin.define.js
+++ etc/webpack-config/plugin/webpack.plugin.define.js
@@ -1,7 +1,11 @@
+/// @see https://webpack.js.org/plugins/environment-plugin/
+
 const { EnvironmentPlugin } = require('webpack')
 
-module.exports = (definitions = {}) => new EnvironmentPlugin({
-    BABEL_ENV: '',
+module.exports = ({
+    definitions = {}
+} = {}) => new EnvironmentPlugin({
+    BABEL_ENV: 'production',
     NODE_ENV: 'production',
     ...definitions
 })
