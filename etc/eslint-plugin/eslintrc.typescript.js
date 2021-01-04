0a0c6b2588f82050754b904472a7504c674ca353
Johannes Pilkahn <johannes@goldeimer.de>
2021-01-04 14:05:33 +0100

    feature(eslint-plugin): (further) integrate typescript

--- etc/eslint-plugin/eslintrc.typescript.js
+++ etc/eslint-plugin/eslintrc.typescript.js
@@ -1,17 +1,18 @@
+const extensions = ['.ts', '.tsx']
+
 module.exports = {
     parser: '@typescript-eslint/parser',
     plugins: ['@typescript-eslint', 'tsc'],
     extends: [
         'plugin:@typescript-eslint/eslint-recommended',
         'plugin:@typescript-eslint/recommended',
+        'airbnb-typescript',
         'plugin:import/typescript'
     ],
     rules: {
         indent: ['off'],
         '@typescript-eslint/indent': ['error', 4],
-        'no-unused-expressions': 'off',
-        '@typescript-eslint/no-unused-expressions': 'warn',
-        'no-useless-constructor': 'off',
+        '@typescript-eslint/no-unused-expressions': 'error',
         '@typescript-eslint/no-useless-constructor': 'error',
         'tsc/config': ['error', {
             configFile: path.resolve(
@@ -22,15 +23,12 @@ module.exports = {
     },
     settings: {
         'import/parsers': {
-            '@typescript-eslint/parser': ['.ts', '.tsx']
+            '@typescript-eslint/parser': extensions
         },
         'import/resolver': {
             typescript: {
                 alwaysTryTypes: true,
-                extensions: [
-                    '.ts',
-                    '.tsx'
-                ],
+                extensions,
                 project: [
                     'etc/*/tsconfig.json',
                     'lib/*/tsconfig.json',
