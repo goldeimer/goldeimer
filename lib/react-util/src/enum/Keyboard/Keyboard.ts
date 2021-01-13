29bd13d96ff0746fb69d1b9c0451cfdcc0ef13bf
Johannes Pilkahn <johannes@goldeimer.de>
2021-01-13 16:33:29 +0100

    refactor(react-util): integrate typescript into build, transform enums to typescript, move LoadingState enum here (for now)

---
+++ lib/react-util/src/enum/Keyboard/Keyboard.ts
@@ -0,0 +1,7 @@
+export enum Keyboard {
+    ArrowDown,
+    ArrowLeft,
+    ArrowRight,
+    ArrowUp,
+    Enter
+}
---
+++ lib/react-util/src/enum/LoadingState/LoadingState.ts
@@ -0,0 +1,5 @@
+export enum LoadingState {
+    Error,
+    Idle,
+    Pending
+}
